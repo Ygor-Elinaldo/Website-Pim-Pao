c98cdde982ceb9bf7a815dffad5d6a0cec0cb173
Ygor Elinaldo Neres de Souza <ygor.elinaldoneresdesouza@gmail.com>
2024-01-28 18:56:13 -0400

    Start of the Project
    Almost complete with just the food menu to be completed

---
+++ Pim Pão/node.js
@@ -0,0 +1,24 @@
+// Importando o módulo express
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Configuração da porta
+const port = 3000;
+
+
+app.use('/resources', express.static(path.join(__dirname, 'resources')));
+
+// Rota padrão
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/cardapio', (req, res) => {
+  res.sendFile(path.join(__dirname, 'cardapio.html'))
+})
+
+// Iniciando o servidor Express
+app.listen(port, () => {
+  console.log(`Servidor Express rodando em http://localhost:${port}`);
+});
