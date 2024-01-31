761dd408ce7b46bb9db34d9c9ea24446165bc8a0
Ygor Elinaldo Neres de Souza <ygor.elinaldoneresdesouza@gmail.com>
2024-01-30 22:00:47 -0400

    Update node.js
    Logs for debug purpose

--- node.js
+++ node.js
@@ -11,6 +11,7 @@ app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
 // Rota padrão
 app.get('/', (req, res) => {
+  console.log("someone is seeing this")
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
@@ -18,3 +19,26 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor Express rodando em http://localhost:${port}`);
 });
+
+app.use((req, res) => {
+  const ip = req.ip || req.connection.remoteAddress;
+  console.log(`Recebida uma solicitação de ${ip}`);
+  res.sendFile(path.join(__dirname, 'index.html'));  
+});
+
+//Debug via LAN
+function getIPAddress() {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
+    }
+  }
+  return '0.0.0.0';
+}
+console.warn(`http://`+getIPAddress()+`:${port}`)
+
